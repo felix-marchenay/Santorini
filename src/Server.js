139885db6aa174c0fa44b2674d3c2f36e147057d
Felix Marchenay <felix.marchenay@opera-energie.com>
2020-05-18 19:16:54 +0200

    server prod

--- src/Server.js
+++ src/Server.js
@@ -6,7 +6,7 @@ export class Server
 {
     constructor() {
         this.emitter = new Emitter;
-        this.host = 'localhost';
+        this.host = 'felixmarchenay.fr';
         this.port = '4949'
         this.socket = null;
         this.transitEvents = [
@@ -16,7 +16,7 @@ export class Server
 
     connect (roomName) {
         const fn = (resolve, reject) => {
-            this.socket = io('http://' + this.host + ':' + this.port);
+            this.socket = io('https://' + this.host + ':' + this.port);
     
             this.socket.on('connected', () => {
                 this.socket.emit('register', roomName);
